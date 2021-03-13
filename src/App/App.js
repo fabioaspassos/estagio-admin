31d39ae3d0585a9a36eb30ac2e85dad53b9db6ef
Fabio Passos <phabiopassos@gmail.com>
2021-03-13 12:06:20 -0300

    customization  of MUI Components and Theme

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import './App.css';
 import SideMenu from '../components/SideMenu'
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
 import Header from '../components/Header';
+import PageHeader from '../components/PageHeader';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 
+const theme = createMuiTheme({
+  palette:{
+    primary:{
+      main: '#333996',
+      light: '#3c44b126'
+    },
+    secondary:{
+      main: '#f83245',
+      light: '#f8324526'
+    },
+    background:{
+      default: '#f4f5fd'
+    },
+  },
+  overrides:{
+    MuiAppBar:{
+      root: {
+        transform:'translateZ(0)'
+      }
+    }
+  },
+  props:{
+    MuiIconButton:{
+      disableRipple: true
+    }
+  }
+})
 
 const useStyles = makeStyles({
   appMain: {
@@ -15,13 +44,17 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
+        <PageHeader 
+          title='Page Header'
+          subTitle='Page description'
+          icon={ <PeopleAltIcon fontSize='large'/> } />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 
